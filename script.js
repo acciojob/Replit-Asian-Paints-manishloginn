60a14456db25489281d6ad6db2cfd73bd0cb35a3
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-16 20:00:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.querySelector("#change_button").addEventListener("click", () => {
+  const blockInput = document.querySelector("#block_id").value;
+  const colorInput = document.querySelector("#colour_id").value;
+ 
+  document
+    .querySelectorAll(".grid-item")
+    .forEach((e) => (e.style.backgroundColor = "transparent"));
+ 
+  const selectedGrid = document.getElementById(blockInput);
+  if (selectedGrid) {
+    selectedGrid.style.backgroundColor = colorInput;
+    selectedGrid.style.transition = "all 0.5s ease-in-out";
+  }
+});
+ 
+document.querySelector("#reset_button").addEventListener("click", () => {
+  document
+    .querySelectorAll(".grid-item")
+    .forEach((e) => (e.style.backgroundColor = "transparent"));
+});
